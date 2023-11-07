9f71a93f0ea3c9700fd2024f2c9aec0a5ce4a109
alexys <alexisaglar@gmail.com>
2023-11-07 17:36:22 +0000

    created BPSO with variable load and generation

--- data/optimisation/optimisation_2.py
+++ data/optimisation/optimisation_2.py
@@ -16,14 +16,12 @@ load_bus_indices = np.arange(len(ppc['bus']))
 # Create Generation and Load Profiles (example, replace with actual data)
 hours = 24
 load = [0.1, 0.18, 0.2, 0.18, 0.15, 0.24, 0.5, 0.6, 0.55, 0.48, 0.45, 0.42, 0.4, 0.4, 0.5, 0.6, 0.8, 0.85, 0.9, 0.8, 0.7, 0.6, 0.3, 0.2] 
-load_profile = np.array(load).reshape(-1, 1) / len(ppc['bus'])
+load_profile = np.array(load).reshape(-1, 1) /50
+print(load_profile)
 
 generation = [0, 0, 0, 0, 0, 0, 0.25, 1, 2, 3.25, 4.5, 5, 4.5, 3.25, 2, 1, 0.5, 0.25, 0, 0, 0, 0, 0, 0]  
-generation_profile = np.array(generation).reshape(-1, 1) / len(ppc['bus'])
-
-  # Define the number of particles and dimensions
-n_particles = 20
-dimensions = len(load_bus_indices)
+generation_profile = np.array(generation).reshape(-1, 1) /20
+print(generation_profile)
 
 class BinaryPSO:
     def __init__(self, n_particles, dimensions, objective_func, max_iter=100):
@@ -88,29 +86,32 @@ def check_constraints(result):
 # Objective function considering generation and load profiles
 def objective_function(x):
     total_power_losses = 0
-    for hour in range(hours):
+    for hour in range(24):  # Assuming 24 hours for daily profiles
         ppc_temp = deepcopy(ppc)
-        # Adjust load and generation for the current hour
-        for bus_idx, load_factor in enumerate(load_profile[hour]):
-            ppc_temp['bus'][bus_idx, 2] *= load_factor
-        for gen_idx, gen_factor in enumerate(generation_profile[hour]):
-            ppc_temp['gen'][gen_idx, 1] *= gen_factor
-        
-        # Add PV penetration at the chosen bus (if applicable)
-        best_bus_idx = np.argmax(x)
-        if x[best_bus_idx] == 1:
-            ppc_temp['bus'][load_bus_indices[best_bus_idx], 2] -= 0.1
-        
+
+        # Adjust load and subtract generation for the current hour at each bus
+        for bus_idx in load_bus_indices:
+            load_change = ppc_temp['bus'][bus_idx, 2] * (load_profile[hour]+1)
+            generation_change = 0
+            # If this bus is allowed to have generation, subtract the generation profile
+            if x[bus_idx] == 1:
+                generation_change = generation_profile[hour]  # Assuming same generation profile for all buses with generation
+            net_load = load_change - generation_change
+            ppc_temp['bus'][bus_idx, 2] = net_load
+
         # Run power flow
         result, success = runpf(ppc_temp, ppopt)
+        
         # Calculate power losses
         if success and check_constraints(result):
             power_losses = sum(result['branch'][:, 13])  # Sum of real power losses in all branches
             total_power_losses += power_losses
         else:
-            return float('inf')  # Return a large number if constraints are violated or power flow fails
-    
+            # Return a large number if constraints are violated or power flow fails
+            return float('inf') 
+
     return total_power_losses
+
 
 # PSO parameters
 n_particles = 20
@@ -120,57 +121,116 @@ max_iter = 50
 # Initialize and run BPSO
 bpso = BinaryPSO(n_particles, dimensions, objective_function, max_iter)
 best_position, best_value = bpso.run(max_iter)
+print(best_position)
 
-# Apply the best PV penetration to the system for each hour and record total power losses
-total_power_losses_over_time = []
-voltages_over_time = []
+total_losses = []
+total_losses_without_pv = []
+total_load_consumption = []
+voltage_profiles = [[] for _ in range(len(ppc['bus']))]
+voltage_profiles_without_pv = [[] for _ in range(len(ppc['bus']))]
+
 for hour in range(hours):
     ppc_temp = deepcopy(ppc)
-    # Adjust load and generation for the current hour
-    for bus_idx, load_factor in enumerate(load_profile[hour]):
-        ppc_temp['bus'][bus_idx, 2] *= load_factor
-    for gen_idx, gen_factor in enumerate(generation_profile[hour]):
-        ppc_temp['gen'][gen_idx, 1] *= gen_factor
-    
-    # Add PV penetration at the chosen bus
-    best_bus_idx = np.argmax(best_position)
-    if best_position[best_bus_idx] == 1:
-        ppc_temp['bus'][load_bus_indices[best_bus_idx], 2] -= 0.1
-    
+    for bus_idx in load_bus_indices:
+        load_change = ppc_temp['bus'][bus_idx, 2] * (load_profile[hour]+1)
+        load_change_without_pv = ppc_temp['bus'][bus_idx, 2] * (load_profile[hour]+1)
+        ppc['bus'][bus_idx, 2] = load_change_without_pv
+        generation_change = 0
+        if best_position[bus_idx] == 1:
+            generation_change = generation_profile[hour]
+        net_load = load_change - generation_change
+        ppc_temp['bus'][bus_idx, 2] = net_load
     # Run power flow
-    result, success = runpf(ppc_temp, ppopt)
-    if success:
-        power_losses = sum(result['branch'][:, 13])  # Sum of real power losses in all branches
-        total_power_losses_over_time.append(power_losses)
-        voltages_over_time.append([bus[7] for bus in result['bus']])
+    result, _ = runpf(ppc_temp, ppopt)
+    result_without_pv, _ = runpf(ppc, ppopt)
+
+    # Store voltage values
+    for bus_idx in load_bus_indices:
+        voltage_profiles[bus_idx].append(result['bus'][bus_idx, 7])
+        voltage_profiles_without_pv[bus_idx].append(result_without_pv['bus'][bus_idx, 7])
+
+    # Sum the real power losses in all branches
+    losses = sum(result['branch'][:, 13] + result['branch'][:, 15])
+    losses_without_pv = sum(result_without_pv['branch'][:, 13] + result_without_pv['branch'][:, 15])
+    total_losses.append(losses)
+    total_losses_without_pv.append(losses_without_pv)
+    total_load_consumption.append(load_change_without_pv)
 
 
-# Plotting
-plt.figure(figsize=(21, 7))
-
-plt.subplot(1, 3, 1)
-plt.bar(load_bus_indices, best_position, color='orange')
-plt.xlabel('Bus Number')
-plt.ylabel('PV Penetration (Binary)')
-plt.title('Optimized PV Penetration at Each Load Bus')
-
-plt.subplot(1, 3, 2)
-plt.plot(range(hours), total_power_losses_over_time, marker='o', color='green')
-plt.xlabel('Hour')
+# Graph Total Power Losses over 24 hours
+plt.figure(figsize=(10, 5))
+plt.plot(total_losses, 'o-')
+plt.plot(total_losses_without_pv, 'o-')
+plt.title('Total Power Losses over 24 Hours')
+plt.xlabel('Hour of the Day')
 plt.ylabel('Total Power Losses (MW)')
-plt.title('Total Power Losses Over Time')
+plt.grid(True)
+plt.show()
 
-plt.subplot(1, 3, 3)
-# Choose an hour to display voltage profile, or take the average
-average_voltages = np.mean(voltages_over_time, axis=0)
-plt.plot(load_bus_indices, average_voltages, marker='o', linestyle='-', color='blue')
+# Now we create the box plots
+plt.figure(figsize=(15, 8))
+plt.boxplot(voltage_profiles)
+plt.title('Voltage Profile Box Plot for Each Bus Over 24 Hours')
 plt.xlabel('Bus Number')
 plt.ylabel('Voltage (p.u.)')
-plt.title('Voltage Profile at Each Bus')
+plt.xticks(np.arange(1, len(ppc['bus']) + 1), np.arange(1, len(ppc['bus']) + 1))  # Ensure that x-ticks match the bus numbers
+plt.grid(True)
+plt.show()
 
+hours = np.arange(1, 25)  # 24-hour period
+total_pv_generation = [sum(g) for g in generation_profile]  # Summing up generation for all PV systems per hour
+# total_load_consumption = [sum(l) for l in load_profile]  # Summing up load for all buses per hour
+
+#plot both generation and consumption in during the day
+plt.figure(figsize=(12, 6))
+# Plotting generation
+plt.plot(hours, total_pv_generation, label='PV Generation (MW)', color='green', marker='o')
+# Plotting load consumption
+plt.plot(hours, total_load_consumption, label='Load Consumption (MW)', color='red', marker='x')
+plt.title('PV Generation and Load Consumption over 24 Hours')
+plt.xlabel('Hour of the Day')
+plt.ylabel('Power (MW)')
+plt.xticks(hours)  # Ensure that x-ticks match the hours
+plt.grid(True)
+plt.legend()
+plt.show()
+
+# Plotting
+fig, ax = plt.subplots(figsize=(15, 5))
+
+# Create a bar graph
+bus_numbers = np.arange(1, len(best_position) + 1)  # Bus numbers from 1 to 33
+ax.bar(bus_numbers, best_position, color='green')
+
+# Set the title and labels
+ax.set_title('PV Installation on Buses (from BPSO Optimization)')
+ax.set_xlabel('Bus Number')
+ax.set_ylabel('PV Installed (0 = No, 1 = Yes)')
+
+# Show the plot
+plt.xticks(bus_numbers)  # Ensure all bus numbers are shown
 plt.tight_layout()
 plt.show()
 
+# Now we create the box plots
+plt.figure(figsize=(15, 8))
 
+# Plot with PV
+plt.boxplot(voltage_profiles, positions=np.arange(1, 2*len(voltage_profiles)+1, 2), widths=0.35, patch_artist=True, boxprops=dict(facecolor='blue', color='blue'), medianprops=dict(color='white'), labels=['With PV']*len(voltage_profiles))
 
+# Plot without PV
+plt.boxplot(voltage_profiles_without_pv, positions=np.arange(2, 2*len(voltage_profiles_without_pv)+2, 2), widths=0.35, patch_artist=True, boxprops=dict(facecolor='red', color='red'), medianprops=dict(color='white'), labels=['Without PV']*len(voltage_profiles_without_pv))
 
+plt.title('Voltage Profile Box Plot for Each Bus Over 24 Hours')
+plt.xlabel('Bus Number')
+plt.ylabel('Voltage (p.u.)')
+
+# Custom x-axis labels to include both 'With PV' and 'Without PV'
+ax = plt.gca()
+ax.set_xticks(np.arange(1.5, 2*len(voltage_profiles)+1, 2))
+ax.set_xticklabels(np.arange(1, len(voltage_profiles)+1))
+ax.set_xlim(0, 2*len(voltage_profiles)+1)
+
+plt.grid(True)
+plt.legend(['With PV', 'Without PV'])
+plt.show()
